6c9aee4205a0be0d9e25fc77bb8a578c5a1329d8
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2021-11-02 15:20:27 +0600

    home page route name add

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('fondend.index');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('backend.index');
